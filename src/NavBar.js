86562f459607c3ad9ef697ac9938925d53d16d0b
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-11-17 11:39:28 -0800

    added Navbar component and functionality

---
+++ src/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+import "./NavBar.css";
+
+function NavBar(props) {
+  return (
+    <nav className="NavBar navbar navbar-light bg-light">
+      <NavLink className="navbar-brand" exact to="/">Jobly</NavLink>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <NavLink className="nav-link" exact to="/companies">Companies</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" exact to="/jobs">Jobs</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="btn btn-sm btn-primary" exact to="/profile">Edit Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
